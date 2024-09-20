4190b95f7f70883f10b532a4f92a9c6222cc4127
dlekdms2 <dlekdms_02@naver.com>
2024-09-20 16:06:36 +0900

    [feat]회원가입, 로그인 페이지 생성/메인화면 도면도 첨부
    1. 회원가입 페이지 생성
    2. 로그인 페이지 생성
    2_1. 로그인 및 회원가입 페이지 이동 가능
    3. 메인화면 도면도 첨부

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Main from '../src/pages/Main'; 
-import Figures from '../src/pages/ClassRoom/Figures'; 
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Main from "../src/pages/Main";
+import Figures from "../src/pages/ClassRoom/Figures";
+import Login from "../src/pages/Login/Login";
+import SignUp from "../src/pages/SignUp/SignUp";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/figures" element={<Figures />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,65 @@
+// src/pages/Login.js
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./Login.css"; // 스타일은 필요에 따라 추가
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //id=user, pw=password
+    if (username === "user" && password === "password") {
+      alert("로그인 성공");
+      navigate("/"); //성공시 main으로 navigate
+    } else {
+      alert("로그인 실패. 다시 시도하세요.");
+    }
+  };
+  //SignUp으로 이동하기
+  const handleSignUpClick = () => {
+    navigate("/signup");
+  };
+
+  return (
+    <div className={styles.loginContainer}>
+      <h2>로그인</h2>
+      <form onSubmit={handleSubmit} className={styles.loginForm}>
+        <div className={styles.formGroup}>
+          <label htmlFor="username">사용자 이름:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="password">비밀번호:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className={styles.loginButton}>
+          로그인
+        </button>
+      </form>
+      {/* 회원가입 버튼 추가 */}
+      <p className={styles.signupText}>
+        아직 계정이 없으신가요?{" "}
+        <button onClick={handleSignUpClick} className={styles.signupButton}>
+          회원가입
+        </button>
+      </p>
+    </div>
+  );
+};
+
+export default Login;
