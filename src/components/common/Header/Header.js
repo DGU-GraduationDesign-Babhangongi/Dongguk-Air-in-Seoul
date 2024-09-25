05106d3b7609a3909c8b351e90574c49265b0d53
dlekdms2 <dlekdms_02@naver.com>
2024-09-26 04:26:53 +0900

    [feat] 로고 클릭시 root로 이동 기능
    1. scr폴더 내 ../../Header/Header.js 파일 건드렸습니다
    2. 로고 클릭시 root로 이동할 수 있는 기능 넣음
    3. <Link></Link>로 사용하고 싶었는데 <a>가 함께 딸려와서 logo 크기가 줄어들길래 navigate써서 했습니다
    4. 부디 충돌나지 않기를 바라며🤦‍♂️🙏

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -1,52 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from './Header.module.css';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import styles from "./Header.module.css";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const GoToRoot = () => {
+    navigate("/");
+  }; //navigate 설정
+
   return (
-    <div style={{height : 'clamp(51px, 4vw, 160px)'}}>
+    <div style={{ height: "clamp(51px, 4vw, 160px)" }}>
       <header className={styles.header}>
         <img
-          src={require('../../../assets/images/logo.png')}
+          src={require("../../../assets/images/logo.png")}
           alt="logo"
           className={styles.logo}
-        /> 
+          style={{ cursor: "pointer" }} // 클릭 가능하게
+          onClick={GoToRoot} // 클릭 이벤트로 navigate 실행
+        />
         <nav className={styles.nav}>
           <Link to="/Alarm" className={styles.navLink}>
             <img
-              src={require('../../../assets/images/alarm.png')}
+              src={require("../../../assets/images/alarm.png")}
               alt="alarm"
               className={styles.icon}
-            /> 
-            <div className={styles.font}>
-              알림
-            </div>
+            />
+            <div className={styles.font}>알림</div>
           </Link>
           <Link to="/Log" className={styles.navLink}>
             <img
-              src={require('../../../assets/images/list.png')}
+              src={require("../../../assets/images/list.png")}
               alt="/log"
               className={styles.icon}
-            /> 
-            <div className={styles.font}>
-              강의실 목록
-            </div>
+            />
+            <div className={styles.font}>강의실 목록</div>
           </Link>
           <Link to="/login" className={styles.navLink}>
             <img
-              src={require('../../../assets/images/user.png')}
+              src={require("../../../assets/images/user.png")}
               alt="user"
               className={styles.icon}
-            /> 
-            <div className={styles.font}>
-              로그인
-            </div>
+            />
+            <div className={styles.font}>로그인</div>
           </Link>
         </nav>
       </header>
       <hr className={styles.separator} />
     </div>
   );
-}
+};
 
 export default Header;
