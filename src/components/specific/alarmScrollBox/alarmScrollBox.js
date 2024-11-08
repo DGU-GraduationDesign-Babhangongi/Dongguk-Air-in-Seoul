14592f1bba5f5448a98d43c34391fa7db88bc199
MJ <nampink2300@gmail.com>
2024-11-08 19:40:24 +0900

    [fix] 헤더, 사이드 바 등 css 변경

--- src/components/specific/alarmScrollBox/alarmScrollBox.js
+++ src/components/specific/alarmScrollBox/alarmScrollBox.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import styles from './alarmScrollBox.module.css'; // CSS 모듈 import
 
-const AlarmScrollBox = () => {
-  return (
-    <div className={styles.scrollableContainer}>
-      {/* 스크롤할 내용 */}
-      <div>[2024.07.20 PM 23:56]
-        PM2.5 수치 이상
-      </div>
-      <div>[2024.07.20 PM 23:56]
-        PM2.5 수치 이상
-      </div>
-      <div>[2024.07.20 PM 23:56]
-        PM2.5 수치 이상
-      </div>
-      <div>[2024.07.20 PM 23:56]
-        PM2.5 수치 이상
-      </div>
-      <div>[2024.07.20 PM 23:56]
-        PM2.5 수치 이상
-      </div>
-      <div>[2024.07.20 PM 23:56]
-        PM2.5 수치 이상
-      </div>
+const AlarmScrollBox = ({ title }) => {
+  const formatMessage = (message) => {
+    return message.split("\n").map((line, index) => (
+      <span key={index}>{line}<br /></span>
+    ));
+  };
 
+  return (
+    <div className={styles.Container}>
+      <div style={{ marginLeft: '5%' }}>{title}</div>
+      <div className={styles.scrollableContainer}>
+        {/* 스크롤할 내용 */}
+        <div>{formatMessage("[07.20 PM 23:56]\n PM2.5 수치 이상")}</div>
+        <div>{formatMessage("[07.20 PM 23:56]\n PM2.5 수치 이상")}</div>
+        <div>{formatMessage("[07.20 PM 23:56]\n PM2.5 수치 이상")}</div>
+        <div>{formatMessage("[07.20 PM 23:56]\n PM2.5 수치 이상")}</div>
+        <div>{formatMessage("[07.20 PM 23:56]\n PM2.5 수치 이상")}</div>
+        <div>{formatMessage("[07.20 PM 23:56]\n PM2.5 수치 이상")}</div>
+      </div>
     </div>
   );
 };
