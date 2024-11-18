8eabee75ea49f9926d4735f8b9c4ceb72a5bda3c
MJ <nampink2300@gmail.com>
2024-11-18 20:09:07 +0900

    [feat]  header 로그인에 따라 변하도록

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styles from "./Header.module.css";
 
-const Header = () => {
+const Header = ({ i }) => {
   const navigate = useNavigate();
+  const [token, setToken] = useState(localStorage.getItem("token")); // token 상태 관리
+
+  useEffect(() => {
+    const handleStorageChange = () => {
+      setToken(localStorage.getItem("token"));
+    };
+
+    // localStorage 변경 감지
+    window.addEventListener("storage", handleStorageChange);
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, []);
+
   const GoToRoot = () => {
     navigate("/");
     window.location.reload();
@@ -19,32 +34,61 @@ const Header = () => {
           style={{ cursor: "pointer" }}
           onClick={GoToRoot}
         />
-        <nav className={styles.nav}>
-          <Link to="/Alarm" className={styles.navLink}>
-            <img
-              src={require("../../../assets/images/alarm.png")}
-              alt="alarm"
-              className={styles.icon}
-            />
-            <div className={styles.font}>알림</div>
-          </Link>
-          <Link to="/Log" className={styles.navLink}>
-            <img
-              src={require("../../../assets/images/list.png")}
-              alt="/log"
-              className={styles.icon}
-            />
-            <div className={styles.font}>강의실 목록</div>
-          </Link>
-          <Link to="/login" className={styles.navLink}>
-            <img
-              src={require("../../../assets/images/user.png")}
-              alt="user"
-              className={styles.icon}
-            />
-            <div className={styles.font}>로그인</div>
-          </Link>
-        </nav>
+
+        {!token ? (
+          <div className={styles.noTokenMessage}>
+            <Link to="/login" className={styles.navLink}>
+              <img
+                src={require("../../../assets/images/Header/person.png")}
+                alt="user"
+                className={styles.icon}
+              />
+              <div className={styles.font}>로그인</div>
+            </Link>
+          </div>
+        ) : (
+          <nav className={styles.nav}>
+            <Link to="/alarm" className={styles.navLink}>
+              <img
+                src={require("../../../assets/images/Header/alarm.png")}
+                alt="alarm"
+                className={styles.icon}
+              />
+              <div
+                className={styles.font}
+                style={{ fontWeight: i === "1" ? "bold" : "normal" }}
+              >
+                이상 수치값 조회
+              </div>
+            </Link>
+            <Link to="/log" className={styles.navLink}>
+              <img
+                src={require("../../../assets/images/Header/log.png")}
+                alt="/log"
+                className={styles.icon}
+              />
+              <div
+                className={styles.font}
+                style={{ fontWeight: i === "2" ? "bold" : "normal" }}
+              >
+                강의실 로그
+              </div>
+            </Link>
+            <Link to="/manager" className={styles.navLink}>
+              <img
+                src={require("../../../assets/images/Header/person.png")}
+                alt="user"
+                className={styles.icon}
+              />
+              <div
+                className={styles.font}
+                style={{ fontWeight: i === "3" ? "bold" : "normal" }}
+              >
+                이수민 관리자
+              </div>
+            </Link>
+          </nav>
+        )}
       </header>
       <hr className={styles.separator} />
     </div>
